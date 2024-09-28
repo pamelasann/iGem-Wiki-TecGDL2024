97b5337ab0e63566cd63d3a161a79bc0ae8e316a
Andres Benjamin Antelis Moreno <bantelis10@gmail.com>
2024-09-28 15:41:18 -0600

    FIXED loading screen & experiments table

--- src/contents/experiments.tsx
+++ src/contents/experiments.tsx
@@ -1,19 +1,91 @@
-import { Inspirations, InspirationLink } from "../components";
-
 export function Experiments() {
   return (
     <>
-      <header className="team-header" style={{
-        backgroundImage: `url('https://nsf-gov-resources.nsf.gov/styles/hero_small/s3/2023-04/Andromeda_galaxy_h.jpg.webp?VersionId=ax62fO1jAP93s16R6H_jPKVhTfvwH9X4&itok=WSHuusB5')`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        height: '300px',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center'
-      }}>
+      <header
+        className="team-header"
+        style={{
+          backgroundImage: `url('https://nsf-gov-resources.nsf.gov/styles/hero_small/s3/2023-04/Andromeda_galaxy_h.jpg.webp?VersionId=ax62fO1jAP93s16R6H_jPKVhTfvwH9X4&itok=WSHuusB5')`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          height: '300px',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
         <h1 style={{ color: 'white', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.7)' }}>Experiments</h1>
       </header>
+      {/* Table Section */}
+      <div className="table">
+        <table>
+          <caption>Table 2 Fitting constants</caption>
+          <thead>
+            <tr>
+              <th>Times</th>
+              <th>$C_1$ origin</th>
+              <th>$\gamma$ origin</th>
+              <th>$r$ origin</th>
+              <th>$C_1$ converge</th>
+              <th>$\gamma$ converge</th>
+              <th>$r$ converge</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>0.221747</td>
+              <td>0.117418</td>
+              <td>0.296676</td>
+              <td>0.007803174</td>
+              <td>0.049709744</td>
+              <td>0.140243978</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>0.221747</td>
+              <td>0.117418</td>
+              <td>0.296676</td>
+              <td>0.007803174</td>
+              <td>0.049709744</td>
+              <td>0.140243978</td>
+            </tr>            <tr>
+              <td>1</td>
+              <td>0.221747</td>
+              <td>0.117418</td>
+              <td>0.296676</td>
+              <td>0.007803174</td>
+              <td>0.049709744</td>
+              <td>0.140243978</td>
+            </tr>            <tr>
+              <td>1</td>
+              <td>0.221747</td>
+              <td>0.117418</td>
+              <td>0.296676</td>
+              <td>0.007803174</td>
+              <td>0.049709744</td>
+              <td>0.140243978</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>0.221747</td>
+              <td>0.117418</td>
+              <td>0.296676</td>
+              <td>0.007803174</td>
+              <td>0.049709744</td>
+              <td>0.140243978</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>0.221747</td>
+              <td>0.117418</td>
+              <td>0.296676</td>
+              <td>0.007803174</td>
+              <td>0.049709744</td>
+              <td>0.140243978</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
