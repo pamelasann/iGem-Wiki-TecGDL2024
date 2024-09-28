b7b8e2a5fc05147ed026d8aeb3f00d8faf42d78f
Andres Benjamin Antelis Moreno <bantelis10@gmail.com>
2024-09-28 13:05:03 -0600

    Loading screen 301

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import logo from "../Logo.png"; // Adjust the import path according to your project structure
 
 const Loading = ({ className }: { className?: string }) => {
   return (
     <div className={`loading-screen ${className}`}>
-      <h2>Loading...</h2>
-      {/* You can also add a spinner or animation here */}
+      <img 
+        src={logo} 
+        alt="Loading..." 
+        className="loading-logo" // Added class name
+      />
     </div>
   );
 };
