bc086971adf217c4e8bf45b41820a83e2f3de441
Pamela Sanchez <118239775+pamelasann@users.noreply.github.com>
2024-10-01 18:03:06 -0600

    Added Lab Notebook to Contribution page (#26)

--- src/contents/notebook.tsx
+++ src/contents/notebook.tsx
@@ -5,9 +5,6 @@ export function Notebook() {
     <div className="container-fluid p-3 justify-content-center align-items-center" id="simple-content">
         <div className="col-lg-9 col-md-12" id="main-content-container">
           <div className="container-fluid" id="text-content">
-            <div className="container-fluid p-3 mt-5">
-              <h1 className="mb-3">Project log</h1>
-            </div>
             <div className="container-fluid p-3">
               <h2>Human Practices Notebook Timeline</h2>
               <br></br>
@@ -23,6 +20,16 @@ export function Notebook() {
               <div className="d-flex justify-content-center align-items-center">
                 <iframe src="https://static.igem.wiki/teams/5439/documents/notebook-hardware.pdf" id="pdf-viewer"></iframe>
               </div>
+            </div>
+            <br></br>
+            <br></br>
+            <div className="container-fluid p-3">
+              <h2>Lab Notebook Timeline</h2>
+              <br></br>
+              <div className="d-flex justify-content-center align-items-center">
+                <iframe src="https://static.igem.wiki/teams/5439/documents/notebook-lab.pdf" id="pdf-viewer"></iframe>
+              </div>
+              
             </div>
           </div>
         </div>
