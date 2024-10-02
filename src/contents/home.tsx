2daf323e120b7fa88ee8d01f41895054f77546d1
Andres Benjamin Antelis Moreno <bantelis10@gmail.com>
2024-10-01 22:00:41 -0600

    Boton to raro

--- src/contents/home.tsx
+++ src/contents/home.tsx
@@ -1,25 +1,41 @@
 export function Home() {
   return (
-      <>
+    <>
       <div className="container-fluid" id="home">
         <div className="container-fluid" id="home-container">
           <div className="d-flex justify-content-center align-items-center" id="home-bannerImage">
-            <img src="https://static.igem.wiki/teams/5439/banner/hometext3.svg" id="home-image-front"></img>
-            <img src="https://static.igem.wiki/teams/5439/banner/axolotl.gif" id="home-image-back"></img>
+            <img src="https://static.igem.wiki/teams/5439/banner/hometext3.svg" id="home-image-front" alt="Banner Text" />
+            <img src="https://static.igem.wiki/teams/5439/banner/axolotl.gif" id="home-image-back" alt="Axolotl Animation" />
           </div>
         </div>
         <div className="container-fluid p-4 m-4"></div>
         <div className="container-fluid p-4 m-4"></div>
 
+        {/* Add the .go element here */}
+        <div className="d-flex justify-content-center align-items-center my-4">
+          <div className="go">
+            <p>Let's start the journey!</p>
+            <div className="btn">
+              <div className="line"></div>
+            </div>
+          </div>
+        </div>
+
+
+
         <div className="container-fluid p-4 m-4 d-flex justify-content-center align-items-center">
           <div className="col-12 col-lg-2"></div>
-            <div className="col-12 col-lg-8 d-flex justify-content-center align-items-center">
-              <h1><b>By 2050</b>, the global population will increase by <b>2 billion people</b>, placing additional pressure on <b>freshwater resource</b>.</h1>
-            </div>
-            <div className="col-12 col-lg-2"></div>
+          <div className="col-12 col-lg-8 d-flex justify-content-center align-items-center">
+            <h1><b>By 2050</b>, the global population will increase by <b>2 billion people</b>, placing additional pressure on <b>freshwater resources</b>.</h1>
+          </div>
+          <div className="col-12 col-lg-2"></div>
         </div>
-        <div className="d-flex justify-content-center align-items-center p-4" style={{width: '100%'}}>
-            <img src="https://static.igem.wiki/teams/5439/banner/home-image1.jpg" id="home-simple-image"></img>
+
+
+
+
+        <div className="d-flex justify-content-center align-items-center p-4" style={{ width: '100%' }}>
+          <img src="https://static.igem.wiki/teams/5439/banner/home-image1.jpg" id="home-simple-image" alt="Home Image 1" />
         </div>
 
         <div className="container-fluid p-4 m-4"></div>
@@ -27,13 +43,14 @@ export function Home() {
 
         <div className="container-fluid p-4 m-4 d-flex justify-content-center align-items-center">
           <div className="col-12 col-lg-2"></div>
-            <div className="col-12 col-lg-8 d-flex justify-content-center align-items-center">
-              <h1><b>92% of the world's water</b> is used by <b>agriculture</b>, putting the <b>sustainability of food systems</b> at risk due to growing competition for water.</h1>
-            </div>
-            <div className="col-12 col-lg-2"></div>
+          <div className="col-12 col-lg-8 d-flex justify-content-center align-items-center">
+            <h1><b>92% of the world's water</b> is used by <b>agriculture</b>, putting the <b>sustainability of food systems</b> at risk due to growing competition for water.</h1>
+          </div>
+          <div className="col-12 col-lg-2"></div>
         </div>
-        <div className="d-flex justify-content-center align-items-center p-4" style={{width: '100%'}}>
-            <img src="https://static.igem.wiki/teams/5439/banner/home-image2.jpg" id="home-simple-image"></img>
+
+        <div className="d-flex justify-content-center align-items-center p-4" style={{ width: '100%' }}>
+          <img src="https://static.igem.wiki/teams/5439/banner/home-image2.jpg" id="home-simple-image" alt="Home Image 2" />
         </div>
 
         <div className="container-fluid p-4 m-4"></div>
@@ -41,13 +58,14 @@ export function Home() {
 
         <div className="container-fluid p-4 m-4 d-flex justify-content-center align-items-center">
           <div className="col-12 col-lg-2"></div>
-            <div className="col-12 col-lg-8 d-flex justify-content-center align-items-center">
-              <h1><b>That's why wastewater reuse</b> comes as an urgent measure, but <b>80% of global wastewater</b> is not adequately treated, and <b>11% of croplands</b> are irrigated with this water, particularly in the <b>Global South</b>.</h1>
-            </div>
-            <div className="col-12 col-lg-2"></div>
+          <div className="col-12 col-lg-8 d-flex justify-content-center align-items-center">
+            <h1><b>That's why wastewater reuse</b> comes as an urgent measure, but <b>80% of global wastewater</b> is not adequately treated, and <b>11% of croplands</b> are irrigated with this water, particularly in the <b>Global South</b>.</h1>
+          </div>
+          <div className="col-12 col-lg-2"></div>
         </div>
-        <div className="d-flex justify-content-center align-items-center p-4" style={{width: '100%'}}>
-            <img src="https://static.igem.wiki/teams/5439/banner/home-image3.jpeg" id="home-simple-image"></img>
+
+        <div className="d-flex justify-content-center align-items-center p-4" style={{ width: '100%' }}>
+          <img src="https://static.igem.wiki/teams/5439/banner/home-image3.jpeg" id="home-simple-image" alt="Home Image 3" />
         </div>
 
         <div className="container-fluid p-4 m-4"></div>
@@ -55,13 +73,14 @@ export function Home() {
 
         <div className="container-fluid p-4 m-4 d-flex justify-content-center align-items-center">
           <div className="col-12 col-lg-2"></div>
-            <div className="col-12 col-lg-8 d-flex justify-content-center align-items-center">
-              <h1>In <b>Mexico</b>, only <b>5.4% of wastewater is reused</b>, and research on <b>emerging contaminants</b> and their <b>environmental risks</b> is limited.</h1>
-            </div>
-            <div className="col-12 col-lg-2"></div>
+          <div className="col-12 col-lg-8 d-flex justify-content-center align-items-center">
+            <h1>In <b>Mexico</b>, only <b>5.4% of wastewater is reused</b>, and research on <b>emerging contaminants</b> and their <b>environmental risks</b> is limited.</h1>
+          </div>
+          <div className="col-12 col-lg-2"></div>
         </div>
-        <div className="d-flex justify-content-center align-items-center p-4" style={{width: '100%'}}>
-            <img src="https://static.igem.wiki/teams/5439/banner/home-image4.jpeg" id="home-simple-image"></img>
+
+        <div className="d-flex justify-content-center align-items-center p-4" style={{ width: '100%' }}>
+          <img src="https://static.igem.wiki/teams/5439/banner/home-image4.jpeg" id="home-simple-image" alt="Home Image 4" />
         </div>
 
         <div className="container-fluid p-4 m-4"></div>
@@ -69,13 +88,14 @@ export function Home() {
 
         <div className="container-fluid p-4 m-4 d-flex justify-content-center align-items-center">
           <div className="col-12 col-lg-2"></div>
-            <div className="col-12 col-lg-8 d-flex justify-content-center align-items-center">
-              <h1>Developing <b>accessible</b> and <b>cost-effective detection techniques</b> becomes a top priority to detect <b>emerging contaminants</b> in the <b>water</b> used in the making of products we end up consuming.</h1>
-            </div>
-            <div className="col-12 col-lg-2"></div>
+          <div className="col-12 col-lg-8 d-flex justify-content-center align-items-center">
+            <h1>Developing <b>accessible</b> and <b>cost-effective detection techniques</b> becomes a top priority to detect <b>emerging contaminants</b> in the <b>water</b> used in the making of products we end up consuming.</h1>
+          </div>
+          <div className="col-12 col-lg-2"></div>
         </div>
-        <div className="d-flex justify-content-center align-items-center p-4" style={{width: '100%'}}>
-            <img src="https://static.igem.wiki/teams/5439/banner/home-image5.jpeg" id="home-simple-image"></img>
+
+        <div className="d-flex justify-content-center align-items-center p-4" style={{ width: '100%' }}>
+          <img src="https://static.igem.wiki/teams/5439/banner/home-image5.jpeg" id="home-simple-image" alt="Home Image 5" />
         </div>
 
         <div className="container-fluid p-4 m-4"></div>
@@ -83,17 +103,15 @@ export function Home() {
 
         <div className="container-fluid p-4 m-4 d-flex justify-content-center align-items-center">
           <div className="col-12 col-lg-2"></div>
-            <div className="col-12 col-lg-8 d-flex justify-content-center align-items-center">
-              <h1><b>LEARN MORE IN OUR PROJECT DESCRIPTION</b></h1>
-            </div>
-            <div className="col-12 col-lg-2"></div>
+          <div className="col-12 col-lg-8 d-flex justify-content-center align-items-center">
+            <h1><b>LEARN MORE IN OUR PROJECT DESCRIPTION</b></h1>
+          </div>
+          <div className="col-12 col-lg-2"></div>
         </div>
 
         <div className="container-fluid p-4 m-4"></div>
         <div className="container-fluid p-4 m-4"></div>
-
-
       </div>
-      </>
+    </>
   );
 }
