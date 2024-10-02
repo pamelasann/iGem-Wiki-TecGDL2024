15b5898533e178252fbaf68a7a2b9e24a96306c0
Pamela Sanchez <118239775+pamelasann@users.noreply.github.com>
2024-10-01 21:59:56 -0600

    Added Education content (#30)

--- src/contents/attributions.tsx
+++ src/contents/attributions.tsx
@@ -23,14 +23,24 @@ export function Attributions() {
 
   return (
     <>
-          {/* Iframe for the attribution form */}
-          <div className="container">
-            <iframe
-              style={{ width: "100%" }}
-              id="igem-attribution-form"
-              src={`https://teams.igem.org/wiki/${teamID}/attributions`}
-            />
+      {/* Simple Content */}
+      <div className="container-fluid p-3 justify-content-center align-items-center" id="simple-content">
+          <div className="col-lg-9 col-md-12" id="main-content-container">
+            <div className="container-fluid" id="text-content">
+              <div className="container-fluid p-3 mt-5">
+                <h1 className="mb-3">Project attributions</h1>
+              </div>
+              {/* Iframe for the attribution form */}
+              <div className="container">
+                <iframe
+                  style={{ width: "100%" }}
+                  id="igem-attribution-form"
+                  src={`https://teams.igem.org/wiki/${teamID}/attributions`}
+                />
+              </div>
+            </div>
           </div>
+        </div>
     </>
   );
 }
